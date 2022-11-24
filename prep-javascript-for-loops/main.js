94791de2c85bfe906e6eb2198c81fe2be66d0e63
Samuel Lee <slee0258@gmail.com>
2022-11-24 00:54:49 +0000

    Completed prep-javascript-for-loops exercise.

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,12 @@
+// create your loops here.
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+for (let j = 0; j < 20; j += 2) {
+  console.log(j);
+}
+
+for (let z = 100; z > -1; z--) {
+  console.log('Time till explosion:', z);
+}
